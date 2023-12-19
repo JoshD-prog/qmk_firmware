2287ce0d12baac9168c7e22aa4d4029ba4ab1462
Joshua Dowd <josh.r.dowd@gmail.com>
2023-12-18 19:12:03 -0600

    update config

---
+++ keyboards/handwired/dactyl_manuform/4x5_5/keymaps/default/secrets.h
@@ -0,0 +1,3 @@
+
+
+static const char *const secrets[] = {"1!ForgetTomorrow\n", "1!GrapeCola\n", "1!WithoutEverLooking\n", "1!SoLongHorse\n", "1!Sacrifice\n", "josh_dowd@americancentury.com", "y2d@americancentury.com", "josh.r.dowd@gmail.com", "mytimepants@gmail.com", "joshandstacia2022@gmail.com"};
